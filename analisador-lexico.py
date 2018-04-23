afaf6a953f1a31449377bb65de8bc30d7f937333
Warley Santos <warleygsantos@hotmail.com>
2018-04-23 19:27:25 -0300

    Abre arquivo e lê caracter

--- analisador-lexico.py
+++ analisador-lexico.py
@@ -4,3 +4,6 @@ FORMAT = '%(asctime)s %(levelname)s:%(name)s:%(lineno)s -> %(message)s'
 logging.basicConfig(filename='analisador-lexico.log',level=logging.DEBUG,format=FORMAT, datefmt='%H:%M:%S')
 
 logging.info('Beging')
+file = open('FONTE.ALG', 'r')
+
+print(file.read(1))
