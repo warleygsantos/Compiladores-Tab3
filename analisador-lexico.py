7b288a0741f7c298336ce8fbda39bfc2ffe02431
Warley Santos <warleygsantos@hotmail.com>
2018-04-24 03:53:03 -0300

    mais algumas implementacoes
    - Dicionario para tokens
    - TAB Space e enter corretamente na matriz
    - funcao que analiza

--- analisador-lexico.py
+++ analisador-lexico.py
@@ -15,10 +15,15 @@ def isNumeral(caracter):
 def isFinalState(number):
 	return number in range(1, 13)
 
+tokes = {
+	1: 'Literal', 2: 'id', 3: 'Comentário', 4: 'EOF', 5:'OPR', 6:'OPR', 7:'OPR', 8:'RCB', 9:'OPM', 10:'AB_P', 11:'FC_P',
+	12:'PT_V', 13:'Num', 14:'Num', 15:'Num'
+}
+
 tabela = {
 # Estado Inicial
 	0: 
-		{'TAB': 0, 'SPACE': 0, 'ENTER': 0, '"': 16, 'L':2, 'D':13, '{':17, 'EOF':4, '=':5, '<':6, '>':7, '-':9, '+':9,
+		{'	': 0, ' ': 0, '\n': 0, '"': 16, 'L':2, 'D':13, '{':17, 'EOF':4, '=':5, '<':6, '>':7, '-':9, '+':9,
 		'*':9, '/':9, '(':10, ')':11, ';':12},
 	2:
 		{'L':2, 'D':2, '_':2},
@@ -34,10 +39,10 @@ tabela = {
 	15:
 		{'D':15},
 	16:
-		{'TAB': 16, 'SPACE': 16, 'ENTER': 16, '"': 1, '.':16, 'L':16, 'D':16, '_':16, '{':16, '}':16, '=':16, '<':16,
+		{'	': 16, ' ': 16, '\n': 16, '"': 1, '.':16, 'L':16, 'D':16, '_':16, '{':16, '}':16, '=':16, '<':16,
 		'>':16, '-':16, '+':16,	'*':16, '/':16, '(':16, ')':16, ';':16, 'e':16, 'E':16},
 	17:
-		{'TAB': 17, 'SPACE': 17, 'ENTER': 17, '"': 17, '.':17, 'L':17, 'D':17, '_':17, '{':17, '}':3, '=':17, '<':17,
+		{'	': 17, ' ': 17, '\n': 17, '"': 17, '.':17, 'L':17, 'D':17, '_':17, '{':17, '}':3, '=':17, '<':17,
 		'>':17, '-':17, '+':17,	'*':17, '/':17, '(':17, ')':17, ';':17, 'e':17, 'E':17},
 	18:
 		{'D':14},
@@ -66,19 +71,11 @@ def leToken():
 		except Exception:
 			continua = False
 			if isFinalState(s):
-				print (string)
-				print ("E estado final")
+				print (string + ' -> ' + tokes[s])
 			else:
 				print ("Nao e estado final")
 
-leToken()
-leToken()
-leToken()
-leToken()
-leToken()
-leToken()
-leToken()
-leToken()
-
+while True:
+	leToken()
 
 #s = tabela[s]['=']
