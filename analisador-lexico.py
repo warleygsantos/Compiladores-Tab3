8bd1d6ca32f1edaf2f4c6468c9cdcbdd4c8c2ce6
Warley Santos <warleygsantos@hotmail.com>
2018-04-24 02:56:52 -0300

    Muitas mudancas
    - Tabela de transicoes (implementado em dicionario)
    - Verifica se caracter lido é literal ou numeral
    -Verifica se estado que parou e final

--- analisador-lexico.py
+++ analisador-lexico.py
@@ -6,4 +6,71 @@ logging.basicConfig(filename='analisador-lexico.log',level=logging.DEBUG,format=
 logging.info('Beging')
 file = open('FONTE.ALG', 'r')
 
-print(file.read(1))
+
+def isLiteral(caracter):
+	return caracter in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def isNumeral(caracter):
+	return caracter in '0123456789'
+
+def isFinalState(number):
+	return number in range(1, 13)
+
+#print(file.read(1))
+s = 0
+
+tabela = {
+# Estado Inicial
+	0: 
+		{'TAB': 0, 'SPACE': 0, 'ENTER': 0, '"': 16, 'L':2, 'D':13, '{':17, 'EOF':4, '=':5, '<':6, '>':7, '-':9, '+':9,
+		'*':9, '/':9, '(':10, ')':11, ';':12},
+	2:
+		{'L':2, 'D':2, '_':2},
+	# Estado 6
+	6:
+		{'=':5, '-':8},
+	7:
+		{'=':5},
+	13:
+		{'.': 18, 'D':13, 'e':19, 'E':19},
+	14:
+		{'D':14, 'e':14, 'E':14},
+	15:
+		{'D':15},
+	16:
+		{'TAB': 16, 'SPACE': 16, 'ENTER': 16, '"': 16, '.':16, 'L':16, 'D':16, '_':16, '{':16, '}':16, '=':16, '<':16,
+		'>':16, '-':16, '+':16,	'*':16, '/':16, '(':16, ')':16, ';':16, 'e':16, 'E':16},
+	17:
+		{'TAB': 17, 'SPACE': 17, 'ENTER': 17, '"': 17, '.':17, 'L':17, 'D':17, '_':17, '{':17, '}':17, '=':17, '<':17,
+		'>':17, '-':17, '+':17,	'*':17, '/':17, '(':17, ')':17, ';':17, 'e':17, 'E':17},
+	18:
+		{'D':14},
+	19:
+		{'D':15, '-':20, '+':20},
+	20:
+		{'D':15}
+}
+
+continua = True
+string = ''
+while(continua):
+	buffe = file.read(1);
+	c = buffe
+
+	if (isLiteral(c)):
+		buffe = 'L'
+	elif (isNumeral(c)):
+		buffe = 'D'
+
+	try:
+		s = tabela[s][buffe]
+		string = string + c
+	except Exception:
+		continua = False
+		if isFinalState(s):
+			print (string)
+			print ("E estado final")
+		else:
+			print ("Nao e estado final")
+
+#s = tabela[s]['=']
