bbfda717a63d6070433f166bf6ea821b1e11391d
Warley Santos <warleygsantos@hotmail.com>
2018-04-24 23:16:28 -0300

    Reconhece Tab, Espaço, Salta

--- analisador-lexico.py
+++ analisador-lexico.py
@@ -24,19 +24,19 @@ def isNumeral(caracter):
 def isFinalState(index):
 	"""Resposável por verificar se o indice corresponde a um Estado Final.
 
-	Os estados finais do DFA implementado são {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	Os estados finais do DFA implementado são {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 21, 22, 23}
 	"""
-	return index in range(1, 13)
+	return index in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 21, 22, 23)
 
 tokes = {
 	1: 'Literal', 2: 'id', 3: 'Comentário', 4: 'EOF', 5:'OPR', 6:'OPR', 7:'OPR', 8:'RCB', 9:'OPM', 10:'AB_P', 11:'FC_P',
-	12:'PT_V', 13:'Num', 14:'Num', 15:'Num'
+	12:'PT_V', 13:'Num', 14:'Num', 15:'Num', 21:'Tab', 22:'Salto', 23:'Espaço'
 }
 
 tabela = {
 # Estado Inicial
 	0: 
-		{'TAB': 0, 'ESPACO': 0, 'ENTER': 0, '"': 16, 'L':2, 'D':13, '{':17, 'EOF':4, '=':5, '<':6, '>':7, '-':9, '+':9,
+		{'Tab': 21, 'Espaço': 23, 'Salto': 22, '"': 16, 'L':2, 'D':13, '{':17, 'EOF':4, '=':5, '<':6, '>':7, '-':9, '+':9,
 		'*':9, '/':9, '(':10, ')':11, ';':12},
 	2:
 		{'L':2, 'D':2, '_':2},
@@ -52,10 +52,10 @@ tabela = {
 	15:
 		{'D':15},
 	16:
-		{'TAB': 16, 'ESPACO': 16, 'ENTER': 16, '"': 1, '.':16, 'L':16, 'D':16, '_':16, '{':16, '}':16, '=':16, '<':16,
+		{'Tab': 16, 'Espaço': 16, 'Salto': 16, '"': 1, '.':16, 'L':16, 'D':16, '_':16, '{':16, '}':16, '=':16, '<':16,
 		'>':16, '-':16, '+':16,	'*':16, '/':16, '(':16, ')':16, ';':16, 'e':16, 'E':16},
 	17:
-		{'TAB': 17, 'ESPACO': 17, 'ENTER': 17, '"': 17, '.':17, 'L':17, 'D':17, '_':17, '{':17, '}':3, '=':17, '<':17,
+		{'Tab': 17, 'Espaço': 17, 'Salto': 17, '"': 17, '.':17, 'L':17, 'D':17, '_':17, '{':17, '}':3, '=':17, '<':17,
 		'>':17, '-':17, '+':17,	'*':17, '/':17, '(':17, ')':17, ';':17, 'e':17, 'E':17},
 	18:
 		{'D':14},
@@ -87,14 +87,14 @@ def leToken():
 		elif (isNumeral(c)):
 			c = 'D'
 		elif (c is '\n'):
-			c = 'ENTER'
+			c = 'Salto'
 			buffe = c
 			linha = linha + 1
 		elif (c is ' '):
-			c = 'ESPACO'
+			c = 'Espaço'
 			buffe = c
 		elif (c is '	'):
-			c = 'TAB'
+			c = 'Tab'
 			buffe = c
 		try:
 			estado = tabela[estado][c]
